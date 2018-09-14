18668505adf9e352b63548fc5501d9891633ed19
billy@arkhamgc.com <billy@arkhamgc.com>
2018-09-14 01:29:29 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,45 @@
+function add(numOne, numTwo) {
+var results = numOne + numTwo;
+
+return results;  
+  
+}
+
+function subtract (numOne, numTwo) {
+var results = numOne - numTwo;
+
+return results;  
+  
+}
+
+function multiply (numOne, numTwo) {
+var results = numOne * numTwo;
+
+return results;  
+  
+}
+
+function divide (numOne, numTwo) {
+var results = numOne / numTwo;
+
+return results;  
+  
+}
+
+function inc (num) {
+var results = num += 1; 
+
+return results;  
+  
+}
+
+function dec (num) {
+var results = num -= 1; 
+
+return results;  
+  
+}
+
+function Int (x, base = 10) {
+  
+}
